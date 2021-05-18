6a0b7d251712f3188afbbe3f29426be6abb5f075
devanshi1500 <devanshi1500@gmail.com>
2021-05-18 14:17:52 +0530

    404 page modified

--- src/pages/Error404.vue
+++ src/pages/Error404.vue
@@ -2,18 +2,18 @@
   <div>
     <div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-2">
       <div class="flex flex-col justify-center items-center">
-        <astronaut></astronaut>
+        <img src="../assets/images/online-shopping.jpg" alt="">
       </div>
-      <div class="flex flex-col justify-center pb-16 px-10">
-        <h1 class="font-extrabold text-gray-800 text-15xl">404</h1>
-        <h2 class="text-green-500 text-6xl lg:text-7xl pb-5">Hmm..</h2>
+      <div class="flex flex-col justify-center pb-10 px-10">
+        <!-- <h1 class="font-extrabold text-gray-800 text-15xl">404</h1> -->
+        <h2 class="text-yellow-400 text-6xl lg:text-8xl pb-5">We Looked Everywhere.</h2>
         <p class="lg:text-2xl font-light text-gray-800">
-          It looks like one of the developers fell asleep.<br />
+          It looks like one of the developers went shopping.<br />
           How you got here is a mystery. <br />
           But you can go back
           <router-link
             to="/"
-            class="text-green-500 hover:text-green-400 font-semibold"
+            class="text-yellow-400 hover:text-yellow-300 font-semibold"
             >home</router-link
           >
           .
@@ -23,9 +23,9 @@
   </div>
 </template>
 <script>
-import Astronaut from "../components/Astronaut.vue";
+// import Astronaut from "../components/Astronaut.vue";
 export default {
-  components: { Astronaut }
+  // components: { Astronaut }
 };
 </script>
 
