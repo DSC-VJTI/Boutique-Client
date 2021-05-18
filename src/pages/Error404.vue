0ba98fdbf0438902533a4a707e32e29df1d16dfa
devanshi1500 <devanshi1500@gmail.com>
2021-05-18 14:23:55 +0530

    minor changes

--- src/pages/Error404.vue
+++ src/pages/Error404.vue
@@ -5,8 +5,7 @@
         <img src="../assets/images/online-shopping.jpg" alt="">
       </div>
       <div class="flex flex-col justify-center pb-10 px-10">
-        <!-- <h1 class="font-extrabold text-gray-800 text-15xl">404</h1> -->
-        <h2 class="text-yellow-400 text-6xl lg:text-8xl pb-5">We Looked Everywhere.</h2>
+        <h2 class="font-semibold text-yellow-400 text-6xl lg:text-8xl pb-5">We Looked Everywhere.</h2>
         <p class="lg:text-2xl font-light text-gray-800">
           It looks like one of the developers went shopping.<br />
           How you got here is a mystery. <br />
@@ -22,15 +21,3 @@
     </div>
   </div>
 </template>
-<script>
-// import Astronaut from "../components/Astronaut.vue";
-export default {
-  // components: { Astronaut }
-};
-</script>
-
-<style scoped>
-.text-15xl {
-  font-size: 12rem;
-}
-</style>
