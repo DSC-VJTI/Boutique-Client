702eb49b4cdbfa9e5e520e4e380f92f61dfd146e
devanshi1500 <devanshi1500@gmail.com>
2021-05-27 12:49:01 +0530

    credits page changes

--- src/pages/miscellaneous/Credits.vue
+++ src/pages/miscellaneous/Credits.vue
@@ -2,222 +2,180 @@
   <div
     class="container flex flex-col mx-auto w-11/12 md:w-4/5 items-center justify-center"
   >
-    <h1 class="text-gray-800 text-center text-5xl font-bold font-serif my-8">
+    <h1 class="text-gray-800 text-center text-5xl font-bold font-serif mt-8">
       Credits
     </h1>
-    <p class="w-4/5 text-gray-800 text-center text-xl my-8">
-      This site has been developed by
-      <a class="text-green-600 italic" href="https://www.dscvjti.tech/"
+    <p class="w-4/5 text-gray-800 text-center text-2xl font-light my-8">
+      This website is developed by members of 
+      <a class="text-green-500 italic font-semibold" href="https://www.dscvjti.tech/" target="blank_"
         >DSC-VJTI</a
       >
-      Students.
     </p>
-    <div
-      class="pt-12 mx-auto w-full grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-4 text-center"
-    >
-      <!-- CARD 1-->
-      <div
-        class="bg-gradient-to-t from-gray-900 to-gray-500 h-60 w-full rounded-3xl relative flex flex-col items-center justify-between p-5 transition-all duration-150 mb-20 mx-auto shadow-xl"
-        style="max-width:260px;"
-      >
-        <!-- PROFILE PIC -->
-        <img
-          class="object-cover rounded-full w-28 h-28 shadow-sm absolute -top-10 transform md:scale-110 duration-700"
-          src="../../assets/images/saif.jpeg"
-          alt="Saif's Photo"
-        />
 
-        <!-- TEXTS -->
-        <p class="text-3xl font-semibold text-white mt-20">Saif Kazi</p>
-        <p class="text-lg text-green-400">Team Member</p>
+    <div id="team-container">
+        <div class="grid grid-flow-row grid-rows-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 mx-16 md:m-0">
+            <div class="profile text-center">
+              <div class="img-box">
+                <img src="..\..\assets\images\saif.jpeg" class="img-responsive">
+                <ul>
+                  <li>
+                    <a href="https://www.linkedin.com/in/saif-1204-kazi/" target="blank_"><i class="fab fa-linkedin-in"></i></a>
+                  </li>
+                  <li>
+                    <a href="https://github.com/Saif807380" target="blank_"><i class="fab fa-github"></i></a>
+                  </li>
+                  <li>
+                    <a href="https://twitter.com/saifkazi_" target="blank_"><i class="fab fa-twitter"></i></a>
+                  </li>
+                </ul>
+              </div>
+              <div class="w-full p-4">
+                <h3 class="font-light text-gray-800 text-3xl">Saif Kazi</h3>
+                <p class="font-light text-gray-800 text-lg">Team Member</p>
+              </div>
+            </div>
 
-        <!-- SOCIAL LINKS -->
-        <div class="flex w-full justify-around">
-          <a
-            class="rounded-full w-8 h-8 shadow-sm bg-green-400 hover:scale-125 transition transform duration-200"
-            href="https://github.com/Saif807380"
-          >
-            <img
-              class="w-8 h-8"
-              src="../../assets/images/github.png"
-              alt="Saif's Github"
-            />
-          </a>
-          <a
-            class="rounded-full w-8 h-8 shadow-sm bg-green-400 hover:scale-125 transition transform duration-200"
-            href="https://www.linkedin.com/in/saif-1204-kazi/"
-          >
-            <img
-              class="w-7 h-7 mx-auto mt-1"
-              src="../../assets/images/linkedin.png"
-              alt="Saif's Linkedin"
-            />
-          </a>
-          <a
-            class="rounded-full w-8 h-8 shadow-sm bg-green-400 hover:scale-125 transition transform duration-200"
-            href="https://twitter.com/saifkazi_"
-          >
-            <img
-              class="w-6 h-6 mt-1 mx-auto"
-              src="../../assets/images/twitter.png"
-              alt="Saif's Twitter"
-            />
-          </a>
+
+            <div class="profile text-center">
+              <div class="img-box">
+                <img src="..\..\assets\images\devanshi.jpeg" class="img-responsive">
+                <ul>
+                  <li>
+                    <a href="https://www.linkedin.com/in/devanshi-shah-5aab44190" target="blank_"><i class="fab fa-linkedin-in"></i></a>
+                  </li>
+                  <li>
+                    <a href="https://github.com/Devanshi1500" target="blank_"><i class="fab fa-github"></i></a>
+                  </li>
+                  <li>
+                    <a href="https://twitter.com/devanshi1500" target="blank_"><i class="fab fa-twitter"></i></a>
+                  </li>
+                </ul>
+              </div>
+              <div class="w-full p-4">
+                <h3 class="font-light text-gray-800 text-3xl">Devanshi Shah</h3>
+                <p class="font-light text-gray-800 text-lg">Team Member</p>
+              </div>
+            </div>
+
+
+            <div class="profile text-center">
+              <div class="img-box">
+                <img src="..\..\assets\images\parth.jpeg" class="img-responsive">
+                <ul>
+                  <li>
+                    <a href="https://linkedin.com/in/parthshah576" target="blank_"><i class="fab fa-linkedin-in"></i></a>
+                  </li>
+                  <li>
+                    <a href="https://github.com/Parth576" target="blank_"><i class="fab fa-github"></i></a>
+                  </li>
+                  <li>
+                    <a href="http://localhost:8080/credits#" target="blank_"><i class="fab fa-twitter"></i></a>
+                  </li>
+                </ul>
+              </div>
+              <div class="w-full p-4">
+                <h3 class="font-light text-gray-800 text-3xl">Parth Shah</h3>
+                <p class="font-light text-gray-800 text-lg">Team Member</p>
+              </div>
+            </div>
+
+
+            <div class="profile text-center">
+              <div class="img-box">
+                <img src="..\..\assets\images\vishal.jpg" class="img-responsive">
+                <ul>
+                  <li>
+                    <a href="https://www.linkedin.com/in/vishalrameshpatil" target="blank_"><i class="fab fa-linkedin-in"></i></a>
+                  </li>
+                  <li>
+                    <a href="https://github.com/SpecTEviL" target="blank_"><i class="fab fa-github"></i></a>
+                  </li>
+                  <li>
+                    <a href="https://twitter.com/VishalPatil1810" target="blank_"><i class="fab fa-twitter"></i></a>
+                  </li>
+                </ul>
+              </div>
+              <div class="w-full p-4">
+                <h3 class="font-light text-gray-800 text-3xl">Vishal Patil</h3>
+                <p class="font-light text-gray-800 text-lg">Team Member</p>
+              </div>
+            </div>
         </div>
-      </div>
-
-      <!-- CARD 2-->
-      <div
-        class="bg-gradient-to-t from-gray-900 to-gray-500 h-60 w-full rounded-3xl relative flex flex-col items-center justify-between p-5 transition-all duration-150 mb-20 mx-auto shadow-xl"
-        style="max-width:260px;"
-      >
-        <!-- PROFILE PIC -->
-        <img
-          class="object-cover rounded-full w-28 h-28 shadow-sm absolute -top-10 transform md:scale-110 duration-700"
-          src="../../assets/images/devanshi.jpeg"
-          alt="Devanshi's Photo"
-        />
-
-        <!-- TEXTS -->
-        <p class="text-3xl font-semibold text-white mt-20">Devanshi Shah</p>
-        <p class="text-lg text-green-400">Team Member</p>
-
-        <!-- SOCIAL LINKS -->
-        <div class="flex w-full justify-around">
-          <a
-            class="rounded-full w-8 h-8 shadow-sm bg-green-400 hover:scale-125 transition transform duration-200"
-            href="https://github.com/Devanshi1500"
-          >
-            <img
-              class="w-8 h-8"
-              src="../../assets/images/github.png"
-              alt="Devanshi's Github"
-            />
-          </a>
-          <a
-            class="rounded-full w-8 h-8 shadow-sm bg-green-400 hover:scale-125 transition transform duration-200"
-            href="https://www.linkedin.com/in/devanshi-shah-5aab44190"
-          >
-            <img
-              class="w-7 h-7 mx-auto mt-1"
-              src="../../assets/images/linkedin.png"
-              alt="Devanshi's Linkedin"
-            />
-          </a>
-          <a
-            class="rounded-full w-8 h-8 shadow-sm bg-green-400 hover:scale-125 transition transform duration-200"
-            href="https://twitter.com/devanshi1500"
-          >
-            <img
-              class="w-6 h-6 mt-1 mx-auto"
-              src="../../assets/images/twitter.png"
-              alt="Devanshi's Twitter"
-            />
-          </a>
-        </div>
-      </div>
-
-      <!-- CARD 3-->
-      <div
-        class="bg-gradient-to-t from-gray-900 to-gray-500 h-60 w-full rounded-3xl relative flex flex-col items-center justify-between p-5 transition-all duration-150 mb-20 mx-auto shadow-xl"
-        style="max-width:260px;"
-      >
-        <!-- PROFILE PIC -->
-        <img
-          class="object-cover rounded-full w-28 h-28 shadow-sm absolute -top-10 transform md:scale-110 duration-700"
-          src="../../assets/images/parth.jpeg"
-          alt="Parth's Photo"
-        />
-
-        <!-- TEXTS -->
-        <p class="text-3xl font-semibold text-white mt-20">Parth Shah</p>
-        <p class="text-lg text-green-400">Team Member</p>
-
-        <!-- SOCIAL LINKS -->
-        <div class="flex w-full justify-around">
-          <a
-            class="rounded-full w-8 h-8 shadow-sm bg-green-400 hover:scale-125 transition transform duration-200"
-            href="https://github.com/Parth576"
-          >
-            <img
-              class="w-8 h-8"
-              src="../../assets/images/github.png"
-              alt="Parth's Github"
-            />
-          </a>
-          <a
-            class="rounded-full w-8 h-8 shadow-sm bg-green-400 hover:scale-125 transition transform duration-200"
-            href="https://linkedin.com/in/parthshah576"
-          >
-            <img
-              class="w-7 h-7 mx-auto mt-1"
-              src="../../assets/images/linkedin.png"
-              alt="Parth's Linkedin"
-            />
-          </a>
-          <a
-            class="rounded-full w-8 h-8 shadow-sm bg-green-400 hover:scale-125 transition transform duration-200"
-            href="#"
-          >
-            <img
-              class="w-6 h-6 mt-1 mx-auto"
-              src="../../assets/images/twitter.png"
-              alt="Parth's Twitter"
-            />
-          </a>
-        </div>
-      </div>
-
-      <!-- CARD 4-->
-      <div
-        class="bg-gradient-to-t from-gray-900 to-gray-500 h-60 w-full rounded-3xl relative flex flex-col items-center justify-between p-5 transition-all duration-150 mb-20 mx-auto shadow-xl"
-        style="max-width:260px;"
-      >
-        <!-- PROFILE PIC -->
-        <img
-          class="object-cover rounded-full w-28 h-28 shadow-sm absolute -top-10 transform md:scale-110 duration-700"
-          src="../../assets/images/vishal.jpg"
-          alt="Vishal's Photo"
-        />
-
-        <!-- TEXTS -->
-        <p class="text-3xl font-semibold text-white mt-20">Vishal Patil</p>
-        <p class="text-lg text-green-400">Team Member</p>
-
-        <!-- SOCIAL LINKS -->
-        <div class="flex w-full justify-around">
-          <a
-            class="rounded-full w-8 h-8 shadow-sm bg-green-400 hover:scale-125 transition transform duration-200"
-            href="https://github.com/SpecTEviL"
-          >
-            <img
-              class="w-8 h-8"
-              src="../../assets/images/github.png"
-              alt="Vishal's Github"
-            />
-          </a>
-          <a
-            class="rounded-full w-8 h-8 shadow-sm bg-green-400 hover:scale-125 transition transform duration-200"
-            href="https://www.linkedin.com/in/vishalrameshpatil"
-          >
-            <img
-              class="w-7 h-7 mx-auto mt-1"
-              src="../../assets/images/linkedin.png"
-              alt="Vishal's Linkedin"
-            />
-          </a>
-          <a
-            class="rounded-full w-8 h-8 shadow-sm bg-green-400 hover:scale-125 transition transform duration-200"
-            href="https://twitter.com/VishalPatil1810"
-          >
-            <img
-              class="w-6 h-6 mt-1 mx-auto"
-              src="../../assets/images/twitter.png"
-              alt="Vishal's Twitter"
-            />
-          </a>
-        </div>
-      </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+#team-container{
+    overflow: hidden;
+    text-align: center;
+    padding-top: 20px;
+    margin: 0 3%;
+}
+
+#team-container h1{
+    font-size: var(--h1fontsize);
+    font-weight: var(--h1fontweight);
+}
+
+.profile{
+    margin: 10px;
+}
+
+.profile .img-box{
+    opacity: 1;
+    display: block;
+    position: relative;
+}
+
+.img-box::after{
+    content: "";
+    opacity: 0;
+    background-color: rgba(0, 0, 0, 0.4);
+    position: absolute;
+    right: 0;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    border-radius: 9999px;
+    width: 100%;
+}
+
+.img-box:hover::after{
+    opacity: 1;
+}
+
+.img-box:hover ul{
+    opacity: 1;
+}
+
+.profile .img-box img{
+    width: 100%;
+    height: auto;
+    border-radius: 1000px;
+}
+
+.img-box ul{
+    padding: 20px 0;
+    position: absolute;
+    z-index: 1;
+    transform: translateX(-50%);
+    opacity: 0;
+    transition: 0.5s;
+    left: 52.5%;
+    bottom: 0;
+    margin: 0;
+    display: flex;
+    flex-direction: row;
+}
+
+.img-box ul li {
+  font-size: 30px;
+  padding: 10px;
+  color: white;
+}
+
+.img-box ul li:hover {
+  opacity: 0.8;
+}
+</style>
